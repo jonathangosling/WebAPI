8059e2eb916f201e5708735d65a5b9122be61754
Jonathan Gosling <jonathangosling@kubrickgroup.com>
2023-04-24 20:45:54 +0100

    added app.mount to mount the images onto the FastAPI

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from mangum import Mangum
 
 app = FastAPI()   # create FastAPI instance
 handler = Mangum(app) # handler for running on AWS Lambda
+app.mount("/imgs", StaticFiles(directory="imgs"), name='images')
 
 @app.get("/")    # define path operation decorator. Tells the FastAPI that function right below is responsible for handling requests that go to the path "/" using a get operation
 async def root():          # will be run if a get request is made to the path "/" i.e. just the root in this case
