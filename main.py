c812b5697ed18ee0c1b42521a8083487de037826
Jonathan Gosling <jonathangosling@kubrickgroup.com>
2023-06-07 18:06:00 +0100

    edited html paths

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ handler = Mangum(app) # handler for running on AWS Lambda
 
 @app.get("/")    # define path operation decorator. Tells the FastAPI that function right below is responsible for handling requests that go to the path "/" using a get operation
 async def root():          # will be run if a get request is made to the path "/" i.e. just the root in this case
-    with open('./html_files/Front_Page.txt', 'r') as file:
+    with open('/html_files/Front_Page.txt', 'r') as file:
         html_content = file.read().replace('\n','')
     return HTMLResponse(content=html_content, status_code=200)
 
 @app.get("/info")    # define path operation decorator. Tells the FastAPI that function right below is responsible for handling requests that go to the path "/" using a get operation
 async def page():          # will be run if a get request is made to the path "/" i.e. just the root in this case
-    with open('./html_files/Info_Page.txt', 'r') as file:
+    with open('/html_files/Info_Page.txt', 'r') as file:
         html_content = file.read().replace('\n','')
     return HTMLResponse(content=html_content, status_code=200)
 
 @app.api_route("/{path_name:path}", methods=["GET"])
 async def catch_all(request: Request, path_name: str):
-    with open('./html_files/Catch_All_Page.txt', 'r') as file:
+    with open('/html_files/Catch_All_Page.txt', 'r') as file:
         html_content = file.read().replace('\n','')
     return HTMLResponse(content=html_content, status_code=200)
