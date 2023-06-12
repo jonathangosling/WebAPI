1aab07e34ee72ccae7de9a571b6ea5b968e0566f
Jonathan Gosling <jonathangosling@kubrickgroup.com>
2023-06-12 14:30:47 +0100

    cleaned things up, removed the old stuff writing from text files and completed the move to using html files with jinja2

--- dockerfile
+++ dockerfile
@@ -2,6 +2,5 @@ FROM public.ecr.aws/lambda/python:3.8
 COPY ./requirements.txt    /requirements.txt
 COPY ./main.py     /main.py
 COPY ./html_files /html_files
-COPY ./html_files_copy /html_files_copy
 RUN pip install -r /requirements.txt
 CMD ["/main.handler"]
--- main.py
+++ main.py
@@ -1,37 +1,27 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from mangum import Mangum
 
 # essentially, this is importing the html files to be returned as a response
-templates = Jinja2Templates(directory="/html_files_copy")
+# Note the "/" here is necessary when running on Lambda through Docker image!
+templates = Jinja2Templates(directory="/html_files")
 
 app = FastAPI()   # create FastAPI instance
 handler = Mangum(app) # handler for running on AWS Lambda
-#app.mount("/html_files", StaticFiles(directory="/html_files"), name='html_files')
 
 @app.get("/", response_class=HTMLResponse)    # define path operation decorator. Tells the FastAPI that function right below is responsible for handling requests that go to the path "/" using a get operation
 async def root(request: Request):          # will be run if a get request is made to the path "/" i.e. just the root in this case
-    # with open('./html_files/Front_Page.txt', 'r') as file:
-    #     html_content = file.read().replace('\n','')
-    #return HTMLResponse(content=html_content, status_code=200)
     return templates.TemplateResponse("Front_Page.html",
                                       {"request": request})
 
 @app.get("/info", response_class=HTMLResponse)
 async def page(request: Request):
-    # with open('/html_files/Info_Page.txt', 'r') as file:
-    #     html_content = file.read().replace('\n','')
-    #return HTMLResponse(content=html_content, status_code=200)
     return templates.TemplateResponse("Info_Page.html",
                                       {"request": request})
 
 # catch all other paths
 @app.api_route("/{path_name:path}", methods=["GET"], response_class=HTMLResponse)
-async def catch_all(request: Request, path_name: str):
-    # with open('/html_files/Catch_All_Page.txt', 'r') as file:
-    #     html_content = file.read().replace('\n','')
-    #return HTMLResponse(content=html_content, status_code=200)
+async def catch_all(request: Request):
     return templates.TemplateResponse("Catch_All_Page.html",
                                       {"request": request})
