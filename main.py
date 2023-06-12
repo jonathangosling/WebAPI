acf693b33a70ad0dadbdc6988d2d52bbe28bc877
Jonathan Gosling <jonathangosling@kubrickgroup.com>
2023-06-12 13:52:24 +0100

    added jinja2 to requirement.txt

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ async def root():          # will be run if a get request is made to the path "/
     #return HTMLResponse(content=html_content, status_code=200)
     return templates.TemplateResponse("Front_Page.html")
 
-@app.get("/info")    # define path operation decorator. Tells the FastAPI that function right below is responsible for handling requests that go to the path "/" using a get operation
-async def page():          # will be run if a get request is made to the path "/" i.e. just the root in this case
+@app.get("/info")
+async def page():
     with open('/html_files/Info_Page.txt', 'r') as file:
         html_content = file.read().replace('\n','')
     #return HTMLResponse(content=html_content, status_code=200)
